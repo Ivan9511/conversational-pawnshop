3728904b7352e8dac44beb4c417aac59bed1a9f7
Ivan9511 <130027284+Ivan9511@users.noreply.github.com>
2026-02-21 16:29:55 +0500

    feat(npc): add npc_data.gd

---
+++ entities/npc/npc_data.gd
@@ -0,0 +1,9 @@
+class_name NPCData
+extends Resource
+
+@export var id: int = 0
+@export var name := "NPC"
+@export var gender: String = "Male"
+@export var favorite_dere_type: String = "neutral"
+
+@export var sprite: Texture2D
