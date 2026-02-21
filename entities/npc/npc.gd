61e27228756f6cbdf3e95a70c33d89755ec0d4f6
Ivan9511 <130027284+Ivan9511@users.noreply.github.com>
2026-02-21 20:58:07 +0500

    feat(npc): update npc script, add test npc resources

--- entities/npc/npc.gd
+++ entities/npc/npc.gd
@@ -1,6 +1,26 @@
 extends Node2D
+class_name CashboxNPC
+
+@export var data: NPCData : set = set_data
 
 @onready var sprite: Sprite2D = $Sprite
 
+
 func _ready() -> void:
-	pass
+	if not data:
+		push_error("NPC data is missing")
+		return
+		
+	_apply_data()
+
+
+func set_data(value: NPCData) -> void:
+	data = value
+	if not is_node_ready():
+		return
+	_apply_data()
+
+
+func _apply_data() -> void:
+	if sprite and data.sprite:
+		sprite.texture = data.sprite
